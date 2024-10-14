b33974cf544be8b91a2cac5566169d9d5157d122
frankmwangi <frankmwangi8118@gmail.com>
2024-10-14 14:26:46 +0300

    first commit

---
+++ src/main/java/com/mwas/jsonWebToken/JsonWebTokenApplication.java
@@ -0,0 +1,13 @@
+package com.mwas.jsonWebToken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonWebTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonWebTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mwas/jsonWebToken/JsonWebTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwas.jsonWebToken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonWebTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
